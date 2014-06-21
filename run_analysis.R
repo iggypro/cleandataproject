8f9b567dfabcc980ee516e7a681bbad6fee39e3f
iggypro <iprotsenko@iprotsenko-macbookair.roam.corp.google.com>
2014-06-21 18:35:35 +0400

    Script update
    Clean data, but by Activity/Subject attributes are not implemented yet.

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,61 @@
 ## Getting and Cleaning Data - Course Project
 ## June 8th, 2014
 
+## Read data from dataset directory
+path <- getwd()
+
+## Read labels
+activity_labels <- read.table(paste(path,"activity_labels.txt",sep="/"),header=F,sep="")
+features <- read.table(paste(path,"features.txt",sep="/"),header=F,sep="")
+
+## Read test set
+subject_test <- read.table(paste(path,"test/subject_test.txt",sep="/"),header=F,sep="")
+x_test <- read.csv(paste(path,"test/X_test.txt",sep="/"),header=F,sep="")
+y_test <- read.csv(paste(path,"test/y_test.txt",sep="/"),header=F,sep="")
+
+## Read training set
+subject_train <- read.table(paste(path,"train/subject_train.txt",sep="/"),header=F,sep="")
+x_train <- read.csv(paste(path,"train/X_train.txt",sep="/"),header=F,sep="")
+y_train <- read.csv(paste(path,"train/y_train.txt",sep="/"),header=F,sep="")
+
+## Add column names to X vectors
+colnames(x_train) <- as.character(features[,2])
+colnames(x_test) <- as.character(features[,2])
+
+## Add activity labels to Y vectors
+names(y_test) <- "Activity"
+names(y_train) <- "Activity"
+y_test$Activity <- as.factor(y_test$Activity)
+y_train$Activity <- as.factor(y_train$Activity)
+levels(y_test$Activity) <- as.character(activity_labels$V2)
+levels(y_train$Activity) <- as.character(activity_labels$V2)
+
+## Merge X, Y and Subject
+x_test$Activity <- as.factor(y_test$Activity)
+x_test$Subject <- as.factor(subject_test$V1)
+x_train$Activity <- as.factor(y_train$Activity)
+x_train$Subject <- as.factor(subject_train$V1)
+
+## Add only Activity, Subject,MEAN & STD variable to Tidy Dataset
+tidyTest <- data.frame(x_test$Activity,x_test$Subject)
+tidyTest <- cbind(tidyTest,
+              x_test[,which(grepl("mean",names(x_test)))],
+              x_test[,which(grepl("std",names(x_test)))])
+colnames(tidyTest)[1:2] <- c("Activity","Subject")
+
+tidyTrain <- data.frame(x_train$Activity,x_train$Subject)
+tidyTrain <- cbind(tidyTrain,
+               x_train[,which(grepl("mean",names(x_train)))],
+               x_train[,which(grepl("std",names(x_train)))])
+colnames(tidyTrain)[1:2] <- c("Activity","Subject")
+
+## Merge Test and Train
+tidy <- data.frame(rbind(tidyTrain,tidyTest))
+
+
+## Subset by mean & std variables
+## grepl("mean",features[,2])
+
 #You should create one R script called run_analysis.R that does the following. 
 # 1. Merges the training and the test sets to create one data set.
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
